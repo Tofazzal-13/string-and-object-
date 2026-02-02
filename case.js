d28a90e5be45359ade2d95d7802987bb1f54d9d1
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-02 23:01:37 +0600

    string case

---
+++ case.js
@@ -0,0 +1,27 @@
+const school = "ST josheph's high school";
+
+// console.log(school);
+
+const subject = 'chemistry'
+const book = "Chemistry"
+
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log('boi tik ache ekhon tmi porte paro');
+    
+}
+else{
+    console.log("hudai hudai pista ultai lav nai");
+    
+}
+
+const drink = "water"
+const liquid = "   water    ";
+
+if(drink.trim() === liquid.trim()){
+    console.log("pani er opor nam jibon");
+    
+}
+else{
+    console.log("pani te lobon ache khawya jabe nah");
+    
+}
