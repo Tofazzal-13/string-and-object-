cf20f76aa03530c35cdef04ebcdcd558e1d456ab
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-02 22:42:01 +0600

    sting

---
+++ string.js
@@ -0,0 +1,18 @@
+const country = 'Bangladesh';
+const division = "Rajshahi";
+const district = `Natore`;
+const thana = new String("Baraigram")
+
+// console.log(thana);
+
+
+ const numbers = [43,58,85,99,65,32,12];
+//  console.log(numbers.length);
+
+
+ const namePerson = "towfique"
+ console.log(namePerson.length);
+ console.log(namePerson[2]);
+ 
+ 
+ 
