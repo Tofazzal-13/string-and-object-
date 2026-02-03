ffb11f88b2e587888c78ce4517a5ae57b9f235dd
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-03 23:36:52 +0600

    string task2 and task done

--- stringTask.js
+++ stringTask.js
@@ -2,7 +2,37 @@
 
 const sentence = "I am a front end developer";
 const count = sentence.split("a").length - 1;
-console.log(count);
+// console.log(count);
 
  //task 2
 
+const str = "I am A front end developer";
+let count1 = 0;
+
+for(let char of str){
+    if(char === "a" || char === "A"){
+        count1++;
+    }   
+}
+
+// console.log(count1);
+
+// task 3
+const str2 = "apple is a good food for health";
+const strLowercase = str2.toLowerCase();
+
+
+if(
+    strLowercase.includes("a") &&
+    strLowercase.includes("e") &&
+    strLowercase.includes("i") &&
+    strLowercase.includes("o") &&
+    strLowercase.includes("u")
+){
+    console.log("all vowel letter include here");
+}
+
+else{
+    console.log("all vowel are not include here");
+    
+}
