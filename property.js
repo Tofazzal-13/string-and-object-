74d29107796bc2735bc4dfa03e24680ae931b67a
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-03 15:21:54 +0600

    property and value explore in object

---
+++ property.js
@@ -0,0 +1,26 @@
+const person = {
+    name: "sodor uddin",
+    age: 25,
+    profession : "developer",
+    salary: 25000,
+    married: true,
+    "fav places": ["bandorban","sundorban","kuakata"]
+}
+
+// find property using dot notation 
+// console.log(person.profession); 
+const salary = person.salary;
+// console.log(salary);
+
+
+// find property using third bracket 
+// console.log(person["age"]);
+const boyos = person["age"];
+// console.log(boyos);
+
+
+console.log(person["fav places"]);
+
+
+
+
---
+++ value.js
@@ -0,0 +1,13 @@
+const person = {
+    name: "sodor uddin",
+    age: 25,
+    profession : "developer",
+    salary: 25000,
+    married: true,
+    "fav places": ["bandorban","sundorban","kuakata"]
+}
+
+person.salary = 30000;
+person["age"] = 29;
+person["fav places"] = ["maldivs", "pataya", "london"]
+console.log(person);
