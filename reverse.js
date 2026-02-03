24b8ba64ede4951de8ec1322dc7f68545ab3b792
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-03 12:37:20 +0600

    reverse js

---
+++ reverse.js
@@ -0,0 +1,26 @@
+const sentence = "I am learning web dev";
+
+let reverse = " ";
+
+for(const letter of sentence){
+    // console.log(letter);
+    reverse = letter + reverse; 
+    
+}
+
+// console.log(reverse);
+let rev = 0; 
+for(let i = 0; i < sentence.length; i++){
+    // console.log(i);
+    // console.log(sentence[i]);
+    const letter = sentence[i];
+    rev = letter + rev
+}
+
+// console.log(rev);
+
+
+// shortcut
+const rever = sentence.split(" ").reverse().join(" ");
+console.log(rever);
+
